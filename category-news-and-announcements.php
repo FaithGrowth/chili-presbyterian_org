934a2b2c537364e77d89228edb6c6ce2509023f1
Jacob Martella <jacob.martella@att.net>
2014-08-07 15:28:26 -0500

    Next round of edits
    What we fixed in the google chat.

--- category-news-and-announcements.php
+++ category-news-and-announcements.php
@@ -1,4 +1,4 @@
-<?php add_action( 'genesis_before_entry', 'chili_add_custom_cat_image'); ?>
+<?php add_action( 'genesis_before_loop', 'chili_add_custom_cat_image'); ?>
 <?php function chili_add_custom_cat_image() {
 	echo '<img src="/wp-content/themes/chili/images/DSCN6205.png" />';
 } ?>
